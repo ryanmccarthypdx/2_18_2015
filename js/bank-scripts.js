d240fc3d12048e0f607dfeb6a01d4c92b7ed0a83
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-18 13:49:05 -0800

    Make it so vladimir only knocks once

--- js/bank-scripts.js
+++ js/bank-scripts.js
@@ -1,19 +1,24 @@
 var Account = {
-  name: "",
+  moniker: "",
   balance: 0,
   withdraw: function(amount) {
-    this.balance = (this.balance - amount);
+    this.balance = (parseInt(this.balance) - amount).toFixed(2);
   },
   deposit: function(amount) {
-    this.balance = (this.balance + amount);
+    this.balance = (parseInt(this.balance) + amount).toFixed(2);
   }
 };
 
 
 $(function() {
+  var alerted = false;
+  var vladimired = false;
+  $("#balance").hide();
   $("#image").html("<img class='pic' src='img/bg.jpg'>");
   var account = 0;
   $("form#sign-up").submit(function(event){
+    alerted = false;
+    vladimired = false;
     $("#image").empty();
     var moniker = $("input#moniker").val();
     var initialDeposit = parseInt($("input#initialDeposit").val());
@@ -22,26 +27,36 @@ $(function() {
     account.balance = initialDeposit;
     $("input#moniker").val("");
     $("input#initialDeposit").val("");
-    $(".balance").text(account.balance);
+    $("#balance").text(account.balance);
     $("#image").html("<img class='pic' src='img/banker.jpg'>");
 
 
     $("form#transact").submit(function(event){
-
-      $(".balance").empty();
+      event.preventDefault();
+      $("#balance").empty();
       var credit = parseInt($("input#deposit").val()) || 0;
       var debit = parseInt($("input#withdraw").val()) || 0;
       account.withdraw(debit);
       account.deposit(credit);
+      if (vladimired === false) {
+        alert("All transaction is subjekt to $29.95 transakticle fee(s) You no like? Vladimir visit you later")
+        vladimired = true
+      }
+      account.withdraw(29.95);
       $(".balance").text(account.balance);
       $("input#deposit").val("");
       $("input#withdrawal").val("");
-      event.preventDefault();
-    })
+    });
+
+    setInterval(function() {
+      account.withdraw(parseInt(account.balance) * 0.049);
+      if (alerted === false) {
+        alert("All akount is subjeck to 4.9% minutely APRs")
+        alerted = true;
+      }
+      $(".balance").text(account.balance);
+    }, 6000);
 
     event.preventDefault();
   });
-
-
-
 });
